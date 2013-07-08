198a51a6b64e8e8a80389552682d9db2a0ddc1f2
Jeremy Huffman <jeremy@jeremyhuffman.com>
2013-07-07 23:27:28 -0400

    moving resourceT manager out of Dash.Store

--- src/Dash/Store.hs
+++ src/Dash/Store.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
-module Dash.Store(readCommand, writeCommand, getV, putV) where
+module Dash.Store(readCommand, writeCommand, getV, putV, openDB) where
 
 
 import           BasicPrelude
+import qualified Prelude as P
 import           Control.Monad
 import           Control.Monad.IO.Class            (liftIO)
-import           Control.Monad.Trans.Resource      (ResourceT, release)
+import           Control.Monad.Trans.Resource      (release, ResIO)
 
-import qualified Data.ByteString                   as BS
-import qualified Data.ByteString.Lazy              as LBS
+import qualified Data.ByteString.Lazy              as LByteS
 
 import           Data.Default                      (def)
 import           Database.LevelDB
@@ -16,13 +16,13 @@ import           Database.LevelDB
 import qualified Dash.Proto                        as Proto
 import qualified Dash.Proto.Runnable.NagiosCommand as NC
 
-readCommand :: BS.ByteString -> IO NC.NagiosCommand
-readCommand key = runResourceT $
-    openDB >>= (\db -> get db def key) >>= parseCmd
+readCommand :: DB -> ByteString -> ResIO NC.NagiosCommand
+readCommand db key =
+    get db def key >>= parseCmd
   where
     parseCmd Nothing = error "Didn't find value for key"
     parseCmd (Just input) = case Proto.messageGet $ toLazy input of
-        Right (cmd, remain) | LBS.length remain == 0 ->
+        Right (cmd, remain) | LByteS.length remain == 0 ->
             return cmd
         Right (_, _) ->
             error "Failed to parse Command fully."
@@ -30,31 +30,25 @@ readCommand key = runResourceT $
             error $ "Failed to parse Command." ++ error_message
 
 
-writeCommand :: NC.NagiosCommand -> IO ()
-writeCommand cmd = runResourceT $ do
-    db <- openDB
+writeCommand :: DB -> NC.NagiosCommand -> ResIO ()
+writeCommand db cmd = do
     put db def
         (toStrict $ Proto.utf8 $ NC.host cmd)
         (toStrict $ Proto.messagePut cmd)
     return ()
 
-putV :: BS.ByteString -> BS.ByteString -> IO ()
-putV key value = runResourceT $ do
-    db <- openDB
-    put db def key value
-    return ()
+putV :: DB -> ByteString -> ByteString -> ResIO ()
+putV db key value = put db def key value
 
-getV :: BS.ByteString -> IO (Maybe BS.ByteString)
-getV key = runResourceT $ do
-    db <- openDB
-    get db def key
+getV :: DB -> ByteString -> ResIO (Maybe ByteString)
+getV db key = get db def key
 
-openDB :: ResourceT IO DB
-openDB =  open "/tmp/leveltest"
+openDB :: P.FilePath -> ResIO DB
+openDB path =  open path
     defaultOptions{createIfMissing = True, cacheSize= 2048}
 
-toLazy :: BS.ByteString -> LBS.ByteString
-toLazy strBS = LBS.fromChunks [strBS]
+toLazy :: ByteString -> LByteString
+toLazy bs = LByteS.fromChunks [bs]
 
-toStrict :: LBS.ByteString -> BS.ByteString
-toStrict = BS.concat . LBS.toChunks
+toStrict :: LByteString -> ByteString
+toStrict = concat . LByteS.toChunks
--- test/Dash/StoreSpec.hs
+++ test/Dash/StoreSpec.hs
@@ -5,6 +5,7 @@ import           BasicPrelude
 import           Test.Hspec
 import           Dash.Proto
 import           Dash.Store
+import           Control.Monad.Trans.Resource      (release, ResIO, runResourceT)
 import qualified Dash.Proto.Runnable.NagiosCommand as NC
 
 main :: IO ()
@@ -14,15 +15,23 @@ spec :: Spec
 spec =
     describe "Dash.Store" $ do
         it "writes Commands to the DB" $
-            writeCommand checkTCP >>= shouldReturn (return ())
+             withDB writeCommand checkTCP >>= shouldReturn (return())
         it "reads Commands from the DB" $
-            readCommand "jeremyhuffman.com" >>= shouldBe checkTCP
+            withDB readCommand "jeremyhuffman.com" >>= shouldBe checkTCP
         it "will fail to read if key is wrong" $
-            readCommand "notgonnamatch" `shouldThrow` errorCall "Didn't find value for key"
+            withDB readCommand "notgonnamatch" `shouldThrow` errorCall "Didn't find value for key"
         it "can write an arbitrary bytestring" $
-            putV "somekey" "somevalue" >>= shouldReturn (return ())
+            withDB2 putV "somekey" "somevalue" >>= shouldReturn (return ())
         it "will fail to deserialize if data is not a protobuf" $
-            readCommand "somekey" `shouldThrow` anyErrorCall
+            withDB readCommand "somekey" `shouldThrow` anyErrorCall
+
+withDB2 f a b = runResourceT $ do
+    db <- openDB "/tmp/leveltest"
+    f db a b
+
+withDB f a = runResourceT $ do
+    db <- openDB "/tmp/leveltest"
+    f db a
 
 checkTCP = NC.NagiosCommand { NC.command = "/usr/lib/nagios/plugins/check_tcp"
                   , NC.host = "jeremyhuffman.com"
