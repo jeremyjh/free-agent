d9efb85aba5ef5c07b78cdbaf2a3796c452eee38
Jeremy Huffman <jeremy@jeremyhuffman.com>
2013-07-30 22:32:06 -0400

    first round

--- src/Dash/Store.hs
+++ src/Dash/Store.hs
@@ -14,7 +14,7 @@ import           Control.Monad
 import           Control.Monad.IO.Class            (liftIO)
 import           Control.Monad.Trans.Resource      (release, ResIO)
 import           Data.ByteString.Char8             (pack)
-import qualified Data.ByteString                   as BS (replicate)
+import qualified Data.ByteString                   as BS
 import           Data.Default                      (def)
 
 import qualified Crypto.Hash.SHA1                  as SHA1
@@ -30,27 +30,31 @@ import           Dash.Proto                        (ProtoBuf(..), wrap
 -- A simple, efficient scheme to allow scans of partial keys; structure data up to three layers, for example:
 -- ("accounts","domestic", "petroleum", "CustID-3387020-2") - this would enable you to do efficient queries
 -- to retrieve all accounts, all domestic accounts, all domestric petroleum accounts etc.
-data Key = Key ByteString | KeyPair (ByteString, ByteString)
-         | KeyTri (ByteString, ByteString, ByteString)
-         | KeyQuad (ByteString,ByteString,ByteString, ByteString) deriving(Show, Eq)
+data Key = Key ByteString | KeyPair ByteString ByteString
+         | KeyTri ByteString ByteString ByteString
+         | KeyQuad ByteString ByteString ByteString ByteString
+         | KeySpace ByteString Key
+         deriving(Show, Eq)
 
 instance IsString Key where fromString = Key . pack
 
 
-
 -- | Key is up to 4-tuple, padded w/ 0s to become 80 bytes for key scans
---
-hashKey :: Key -> ByteString
-hashKey (Key k) = sha1 k ++ pad 3
-hashKey (KeyPair (k1, k2)) = sha1 k1 ++ sha1 k2 ++ pad 2
-hashKey (KeyTri (k1, k2, k3)) = sha1 k1 ++ sha1 k2 ++ sha1 k3 ++ pad 1
-hashKey (KeyQuad (k1, k2, k3, k4)) = sha1 k1 ++ sha1 k2 ++ sha1 k3 ++ sha1 k4
-
-sha1 :: ByteString -> ByteString
-sha1 = SHA1.hash
-
-pad :: Int -> ByteString
-pad = flip BS.replicate 0 . (*20)
+-- A KeySpace ByteString will be pre-pended to the key and is not hashed
+packKey :: Key -> ByteString
+packKey pKey =
+    case pKey of
+        KeySpace _ _ -> hashKey pKey
+        _            -> hashKey $ KeySpace (pad 1) pKey
+  where
+    hashKey (Key k) = sha1 k ++ pad 3
+    hashKey (KeyPair k1 k2) = sha1 k1 ++ sha1 k2 ++ pad 2
+    hashKey (KeyTri k1 k2 k3) = sha1 k1 ++ sha1 k2 ++ sha1 k3 ++ pad 1
+    hashKey (KeyQuad k1 k2 k3 k4) = sha1 k1 ++ sha1 k2 ++ sha1 k3 ++ sha1 k4
+    hashKey (KeySpace bs k) = padSpace bs ++ hashKey k
+    padSpace = BS.take 20 . (++ pad 1)
+    sha1 = SHA1.hash
+    pad = flip BS.replicate 0 . (*20)
 
 type DB = LDB.DB
 
@@ -73,10 +77,10 @@ fetch db k = liftM decode_found $ get db k
     decode_found (Just bs) = decode $ toLazy bs
 
 put :: DB -> Key -> ByteString -> ResIO ()
-put db k = LDB.put db def $ hashKey k
+put db k = LDB.put db def $ packKey k
 
 get :: DB -> Key -> ResIO (Maybe ByteString)
-get db k = LDB.get db def $ hashKey k
+get db k = LDB.get db def $ packKey k
 
 openDB :: P.FilePath -> ResIO DB
 openDB path =  LDB.open path
