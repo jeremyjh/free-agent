887c5b50418a2649058c5d7f21a825aefd5a3a90
Jeremy Huffman <jeremy@jeremyhuffman.com>
2014-02-26 08:05:32 -0500

    refactor agent handlers a bit

--- src/FreeAgent/Process/ManagedAgent.hs
+++ src/FreeAgent/Process/ManagedAgent.hs
@@ -53,14 +53,14 @@ agentCallHandler :: (NFSerializable a, NFSerializable b, Show a)
                   => (a -> (StateT s Agent) b)
                   -> AgentState s -> SendPort b -> a
                   -> Process (ProcessAction (AgentState s) )
-agentCallHandler f s p c = agentHandler s p (f c) >>= continue
-  where
-    agentHandler (AgentState ctxt state') replyCh ma =
-        withAgent ctxt $ do
-            [qdebug| Processing command: #{c}|]
-            (result, newState) <- runStateT ma state'
-            sendChan replyCh result
-            return $ AgentState ctxt newState
+agentCallHandler fn (AgentState ctxt ustate) replyCh command = do
+   newState <-  withAgent ctxt $ do
+        [qdebug| Processing command: #{command}|]
+        (result, newState) <- runStateT (fn command) ustate
+        sendChan replyCh result
+        return newState
+   continue $ AgentState ctxt newState
+
 
 -- | handle calls that may mutate the State environment in
 -- an EitherT monad
@@ -70,20 +70,19 @@ agentCallHandlerET :: (NFSerializable a, NFSerializable b, NFSerializable e
                   -> AgentState s
                   -> SendPort (Either e b)
                   -> a -> Process (ProcessAction (AgentState s) )
-agentCallHandlerET f s p c = agentHandler s p (f c) >>= continue
+agentCallHandlerET fn (AgentState ctxt ustate) replyCh command = do
+    newState <- withAgent ctxt $ do
+        [qdebug| Processing command: #{command}|]
+        let ema = runEitherT (fn command) >>= logEitherT
+        (result, newState) <- runStateT ema ustate
+        sendChan replyCh result
+        return newState
+    continue $ AgentState ctxt newState
   where
-    agentHandler (AgentState ctxt state') replyCh ma =
-        withAgent ctxt $ do
-            [qdebug| Processing command: #{c}|]
-            let ema = runEitherT ma >>= logEitherT
-            (result, newState) <- runStateT ema state'
-            sendChan replyCh result
-            return $ AgentState ctxt newState
-      where
-        logEitherT left'@(Left reason) = do
-            [qwarn| Processing for #{c} failed with reason: #{reason} |]
-            return left'
-        logEitherT right' = return right'
+    logEitherT left'@(Left reason) = do
+        [qwarn| Processing for #{command} failed with reason: #{reason} |]
+        return left'
+    logEitherT right' = return right'
 
 
 -- | handle calls that do not mutate state out of band in stateful process
@@ -91,18 +90,17 @@ agentAsyncCallHandler :: (NFSerializable a, NFSerializable b, Show a)
                   => (a -> (StateT s Agent) b)
                   -> AgentState s -> SendPort b -> a
                   -> Process (ProcessAction (AgentState s) )
-agentAsyncCallHandler f s p c = spawnStateAgent s p (f c) >> continue s
-  where
-    spawnStateAgent (AgentState ctxt state') port ma = do
-        pid <- spawnAgent ctxt $ do
-            [qdebug| Processing command: #{c}|]
-            threadDelay 1000 -- so we have time to setup a link in parent
-            r <- evalStateT ma state'
-            sendChan port r
-        -- TODO: This will prevent the client call from hanging forever
-        -- on an exception/crash but it would be better for executive to monitor child process
-        -- and send an exit to the sendPortProcessId
-        liftProcess $ linkOnFailure pid
+agentAsyncCallHandler fn state'@(AgentState ctxt ustate) port command = do
+    pid <- spawnAgent ctxt $ do
+        [qdebug| Processing command: #{command}|]
+        threadDelay 1000 -- so we have time to setup a link in parent
+        r <- evalStateT (fn command) ustate
+        sendChan port r
+    -- TODO: This will prevent the client call from hanging forever
+    -- on an exception/crash but it would be better for executive to monitor child process
+    -- and send an exit to the sendPortProcessId
+    liftProcess $ linkOnFailure pid
+    continue state'
 
 -- | handle calls that do not mutate state out of band in stateful process
 -- in an EitherT monad - will log (Left reason) on failure
@@ -137,10 +135,8 @@ agentCastHandler :: (NFSerializable a, Show a)
                  => (a -> (StateT s Agent) ())
                  -> AgentState s -> a
                  -> Process (ProcessAction (AgentState s) )
-agentCastHandler f s c = withStateAgent s (f c) >>= continue
-  where
-    withStateAgent (AgentState ctxt state') ma =
-        withAgent ctxt $ do
-            [qdebug| Processing command: #{c}|]
-            newState <- execStateT ma state'
-            return $ AgentState ctxt newState
+agentCastHandler fn (AgentState ctxt ustate) command = do
+    newState <- withAgent ctxt $ do
+        [qdebug| Processing command: #{command}|]
+        execStateT (fn command) ustate
+    continue $ AgentState ctxt newState
