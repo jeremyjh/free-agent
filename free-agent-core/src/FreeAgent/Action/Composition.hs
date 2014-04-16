63dbe710224345243057ea6ba42722e87d4f0ac3
Jeremy Huffman <jeremy@jeremyhuffman.com>
2014-04-16 08:34:42 -0400

    Plan type * -> * for a Functor

--- free-agent-core/src/FreeAgent/Action/Composition.hs
+++ free-agent-core/src/FreeAgent/Action/Composition.hs
@@ -14,39 +14,59 @@ import           AgentPrelude
 import           FreeAgent.Lenses
 import           FreeAgent.Action
 
-import           Control.Monad.State
 
-data ActionPlan
-  = Exec Action
+data Plan a
+  = Exec a
   | DoNothing
-  | Sequential ActionPlan ActionPlan
-  | Parallel ActionPlan ActionPlan
-  | OnFailure ActionPlan ActionPlan
-  | OnSuccess ActionPlan ActionPlan
+  | Sequential (Plan a) (Plan a)
+  | Parallel (Plan a) (Plan a)
+  | OnFailure (Plan a) (Plan a)
+  | OnSuccess (Plan a) (Plan a)
   deriving (Show, Eq, Typeable, Generic)
 
-newtype Planning a
-  = Planning {unPlanning :: State ActionPlan a}
-        deriving (Functor, Applicative, Monad, MonadState ActionPlan)
+type ActionPlan = Plan Action
 
-toExec :: Actionable a b => a -> ActionPlan
-toExec = Exec . toAction
+planExec :: Actionable a b => a -> ActionPlan
+planExec = Exec . toAction
 
-runPlan :: Planning () -> ActionPlan
-runPlan = snd . flip runState DoNothing . unPlanning
+thenExec :: Actionable a b => ActionPlan -> a -> ActionPlan
+thenExec = combinePlan Sequential
 
-beginPlan :: Actionable a b => a -> Planning ()
-beginPlan = put . toExec
+whileExec :: Actionable a b => ActionPlan -> a -> ActionPlan
+whileExec = combinePlan Parallel
 
-thenExec :: Actionable a b => a -> Planning ()
-thenExec action' = do
-    pre <- get
-    put $ Sequential pre (toExec action')
+onSuccess :: Actionable a b => ActionPlan -> a -> ActionPlan
+onSuccess = combinePlan OnSuccess
+
+onFailure :: Actionable a b => ActionPlan -> a -> ActionPlan
+onFailure = combinePlan OnFailure
+
+combinePlan :: Actionable a b
+             => (ActionPlan -> ActionPlan -> ActionPlan)
+             -> ActionPlan -> a -> ActionPlan
+combinePlan fn plan action' = fn plan (planExec action')
 
 example :: ActionPlan
-example = runPlan $ do
-    beginPlan NoOp
-    thenExec NoOp
+example =
+    planExec NoOp
+    `thenExec` NoOp
+    <> planExec NoOp
+    `whileExec` NoOp
+
+instance Monoid (Plan a) where
+    mempty = DoNothing
+    mappend = Sequential
+
+instance Semigroup (Plan a) where
+    (<>) = mappend
+
+instance Functor Plan where
+    fmap _ DoNothing = DoNothing
+    fmap fn (Exec action') = Exec (fn action')
+    fmap fn (Sequential plan1 plan2) = fmap fn plan1 `Sequential` fmap fn plan2
+    fmap fn (Parallel plan1 plan2) = fmap fn plan1 `Parallel` fmap fn plan2
+    fmap fn (OnSuccess plan1 plan2) = fmap fn plan1 `OnSuccess` fmap fn plan2
+    fmap fn (OnFailure plan1 plan2) = fmap fn plan1 `OnFailure` fmap fn plan2
 
 -- NoOp type is just temporarily here for examples
 data NoOp = NoOp deriving (Show, Eq, Typeable, Generic)
@@ -54,5 +74,5 @@ data NoOp = NoOp deriving (Show, Eq, Typeable, Generic)
 instance Stashable NoOp where key = undefined
 instance Runnable NoOp Result where exec = undefined
 
-deriveSerializers ''ActionPlan
+{-deriveSerializers ''Plan-}
 deriveSerializers ''NoOp
